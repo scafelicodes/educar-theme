45ee2dc560b7dc966af1de9a94963f3fd5fd2145
Gustavo Souza <39910511+scafeli@users.noreply.github.com>
2018-08-01 20:03:05 -0300

    indentation setup

--- header.php
+++ header.php
@@ -6,14 +6,13 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title><?php if(is_home()) { echo bloginfo("name"); echo " - "; echo bloginfo("description"); } else { echo wp_title(" - ", false, right); } ?></title>
 
-
     <!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-WGSMV6P');</script>
-	<!-- End Google Tag Manager -->
+  	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  	})(window,document,'script','dataLayer','GTM-WGSMV6P');</script>
+  	<!-- End Google Tag Manager -->
 
     <meta http-equiv="Lang" content="pt-br">
 	   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
